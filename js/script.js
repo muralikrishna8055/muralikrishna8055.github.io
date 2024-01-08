dc32e3e29f7fc832ddfd49cead9f44a6e64432ea
Murali Krishna <mailstomuralikrishna@gmail.com>
2024-01-08 18:17:54 +0530

    wesite abstract complted

--- js/script.js
+++ js/script.js
@@ -42,11 +42,17 @@ navbar.classList.remove('active');
 
 };
 
-
-/*========== swiper ==========*/
-
-
-/*========== dark light mode ==========*/
-
-
 /*========== scroll reveal ==========*/
+
+ScrollReveal({
+    reset: true,
+    distance:'80px',
+    duration: 2000,
+    delay: 200
+});
+
+
+ScrollReveal().reveal('.home-content, .heading',{origin: 'top'});
+ScrollReveal().reveal('.services-container,.portfolio-box',{origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1,.about-img img',{origin: 'left'});
+ScrollReveal().reveal('.home-content h3, .home-content p,about-content',{origin: 'right'});
